d7f81cf35099fac50933e13bf3f5e952a0e68d04
Chris O'Hara <cohara87@gmail.com>
2017-03-02 18:03:54 +1300

    Sketch out the remaining writer methods

--- src/main/java/zcs/jni/Writer.java
+++ src/main/java/zcs/jni/Writer.java
@@ -51,9 +51,91 @@ public class Writer implements AutoCloseable {
         ptr = 0;
     }
 
+    /**
+     * Add a column.
+     *
+     * @param type             Type of the column
+     * @param encoding         Encoding of the column
+     * @param compression      Compression algorithm to use when writing column data
+     * @param compressionLevel Compression level
+     * @throws Exception
+     */
+    public void addColumn(ColumnType type, ColumnEncoding encoding, ColumnCompression compression,
+                          int compressionLevel) throws Exception {
+        nativeAddColumn(ptr, type.ordinal(), encoding.ordinal(), compression.ordinal(),
+                compressionLevel);
+    }
+
+    /**
+     * Write a null value to the specified column.
+     *
+     * @param index Column index
+     * @throws Exception
+     */
+    public void putNull(int index) throws Exception {
+        nativePutNull(ptr, index);
+    }
+
+    /**
+     * Write a boolean value to the specified column.
+     *
+     * @param index Column index
+     * @param value Boolean value to write
+     * @throws Exception
+     */
+    public void putBoolean(int index, boolean value) throws Exception {
+        nativePutBoolean(ptr, index, value);
+    }
+
+    /**
+     * Write an int value to the specified column.
+     *
+     * @param index Column index
+     * @param value Int value to write
+     * @throws Exception
+     */
+    public void putInt(int index, int value) throws Exception {
+        nativePutInt(ptr, index, value);
+    }
+
+    /**
+     * Write a long value to the specified column.
+     *
+     * @param index Column index
+     * @param value Long value to write
+     * @throws Exception
+     */
+    public void putLong(int index, long value) throws Exception {
+        nativePutLong(ptr, index, value);
+    }
+
+    /**
+     * Write a string value to the specified column.
+     *
+     * @param index Column index
+     * @param value String value to write
+     * @throws Exception
+     */
+    public void putString(int index, String value) throws Exception {
+        nativePutString(ptr, index, value);
+    }
+
     private native long nativeNew(String path, long rowGroupSize);
 
     private native void nativeFree(long ptr);
 
     private native void nativeFinish(long ptr, boolean sync);
+
+    private native void nativeAddColumn(long ptr, int type, int encoding, int compression,
+                                        int compressionLevel);
+
+    private native void nativePutNull(long ptr, int index);
+
+    private native void nativePutBoolean(long ptr, int index, boolean value);
+
+    private native void nativePutInt(long ptr, int index, int value);
+
+    private native void nativePutLong(long ptr, int index, long value);
+
+    private native void nativePutString(long ptr, int index, String value);
 }
